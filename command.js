47cf71132154b5d3d9140783adcfc703a23107ef
Glend Maatita <glend.maatita@gmail.com>
2024-04-15 13:57:07 +0700

    add build docs

--- command.js
+++ command.js
@@ -1,8 +1,17 @@
+/**
+ * Executes a CPU-intensive task of calculating prime numbers and calls a callback function when done.
+ * @param {Function} callback - The callback function to be called when the task is completed.
+ */
 function cpuIntensiveTask(callback) {
   const primeNumbers = calculatePrimeNumbers(30000);
   callback(`CPU-intensive task completed. Prime numbers: ${primeNumbers.length}`);
 }
 
+/**
+ * Calculates prime numbers up to a given limit.
+ * @param {number} limit - The upper limit for the prime numbers calculation.
+ * @returns {Array} An array of prime numbers up to the given limit.
+ */
 function calculatePrimeNumbers(limit) {
   const primes = [];
   for (let i = 2; i <= limit; i++) {
@@ -13,6 +22,11 @@ function calculatePrimeNumbers(limit) {
   return primes;
 }
 
+/**
+ * Checks if a given number is prime.
+ * @param {number} num - The number to check.
+ * @returns {boolean} True if the number is prime, false otherwise.
+ */
 function isPrime(num) {
   for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
      if (num % i === 0) {
